e36587c0e7e4ba5c45499aa3aa2b37887b7ee664
eugeniochiau <eugeniochiauchiau@gmail.com>
2022-12-14 18:39:46 +0200

    remocao de imagens desnecessarias

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
     setTimeout(function(){
         // container.remove(carregamento);
         carregamento.setAttribute("class"," loading d-none");
-        navbar.setAttribute("class","navbar navbar-expand-md fixed-top ")
+        navbar.setAttribute("class","navbar navbar-expand-lg fixed-top ")
         main.setAttribute("class","main-content mt-5");
     // console.log(carregamento);  
     },3450);
@@ -36,20 +36,30 @@ function dowload(){
 }
 
 // troca de imagens
-    minato=document.querySelector('.minato');
-    kunai1=document.querySelector('.img-kunai-1');
-    kunai2=document.querySelector('.img-kunai-2');
+    // minato=document.querySelector('.minato');
+    // kunai1=document.querySelector('.img-kunai-1');
+    // kunai2=document.querySelector('.img-kunai-2');
     kunai1.addEventListener('mouseover',function(){ 
-        this.src="img/minato (2).png";
+    this.src="img/naruto/ligth-mode/minato (2).png";
         this.classList.remove('kunai-70');
-        minato.src="img/kunai.png";
-        minato.classList.add('');
-    });
-    kunai1.addEventListener('mouseout',function(){
-        this.src="img/kunai.png";
+        minato.src="img/naruto/ligth-mode/kunai.png";
+        minato.classList.add("kunai-width");
+        setTimeout(function(){
+            kunai1.src="img/naruto/ligth-mode/kunai.png";
         kunai1.classList.add('kunai-70');
-        minato.src="img/minato (2).png";
+        minato.src="img/naruto/ligth-mode/minato (2).png";
+        minato.classList.remove("kunai-width");
+        },1000);
+        
+        // minato.classList.remove("img-fluid");
+        
     });
+    // kunai1.addEventListener('mouseout',function(){
+    //     this.src="img/naruto/ligth-mode/kunai.png";
+    //     kunai1.classList.add('kunai-70');
+    //     minato.src="img/naruto/ligth-mode/minato (2).png";
+    //     minato.classList.remove("kunai-width");
+    // });
     kunai2.addEventListener('mouseover',function(){
 
     });
