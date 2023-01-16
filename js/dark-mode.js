a1b9660aee98b1824dbb9b9f55edd7103d22d395
eugeniochiau <eugeniochiauchiau@gmail.com>
2023-01-16 13:22:35 +0200

    melhorias de dark mode

---
+++ js/dark-mode.js
@@ -0,0 +1,2 @@
+var mode =document.getElementById("mode");
+
--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
   var navbar=document.querySelector(".navbar");
+  const mode=document.querySelector(".btn-mode");
 // antes do carregamento
    (function(){
     var main=document.querySelector(".main-content");
-  
+    
     // var container=document.querySelector(".loading");
     var carregamento=document.querySelector(".loading");
     setTimeout(function(){
         // container.remove(carregamento);
         carregamento.setAttribute("class"," loading d-none");
-        navbar.setAttribute("class","navbar navbar-expand-lg fixed-top ")
+        navbar.setAttribute("class","navbar navbar-expand-lg fixed-top");
         main.setAttribute("class","main-content mt-5");
+        mode.classList.remove('d-none');
+
     // console.log(carregamento);  
     },3450);
     
@@ -18,8 +21,8 @@
 //    navbar transparente
 window.addEventListener('scroll', function(){
     if(this.window.pageYOffset>100)
-       navbar.classList.add('scrolled');
-    else navbar.classList.remove('scrolled');
+    navbar.classList.add('scrolled','shadow-sm');
+    else navbar.classList.remove('scrolled','shadow-sm');
 });
 
 // dowload de CV
@@ -39,27 +42,69 @@ function dowload(){
     // minato=document.querySelector('.minato');
     // kunai1=document.querySelector('.img-kunai-1');
     // kunai2=document.querySelector('.img-kunai-2');
-    kunai1.addEventListener('mouseover',function(){ 
-    this.src="img/naruto/ligth-mode/minato (2).png";
-        this.classList.remove('kunai-70');
-        minato.src="img/naruto/ligth-mode/kunai.png";
-        minato.classList.add("kunai-width");
-        setTimeout(function(){
-            kunai1.src="img/naruto/ligth-mode/kunai.png";
-        kunai1.classList.add('kunai-70');
-        minato.src="img/naruto/ligth-mode/minato (2).png";
-        minato.classList.remove("kunai-width");
-        },1000);
+    // kunai1.addEventListener('mouseover',function(){ 
+    // this.src="img/naruto/ligth-mode/minato (2).png";
+    //     this.classList.remove('kunai-70');
+    //     minato.src="img/naruto/ligth-mode/kunai.png";
+    //     minato.classList.add("kunai-width");
+    //     setTimeout(function(){
+    //         kunai1.src="img/naruto/ligth-mode/kunai.png";
+    //     kunai1.classList.add('kunai-70');
+    //     minato.src="img/naruto/ligth-mode/minato (2).png";
+    //     minato.classList.remove("kunai-width");
+    //     },1000);
         
-        // minato.classList.remove("img-fluid");
+    //     // minato.classList.remove("img-fluid");
         
-    });
+    // });
     // kunai1.addEventListener('mouseout',function(){
     //     this.src="img/naruto/ligth-mode/kunai.png";
     //     kunai1.classList.add('kunai-70');
     //     minato.src="img/naruto/ligth-mode/minato (2).png";
     //     minato.classList.remove("kunai-width");
     // });
-    kunai2.addEventListener('mouseover',function(){
+    // kunai2.addEventListener('mouseover',function(){
 
+    // });
+
+
+
+    // troca para dark mode
+    mode.addEventListener('click',(event)=>{
+        var body=document.querySelector("body");
+        var img_mode=document.querySelector(".img-mode");
+        var img_folha=document.querySelector(".img-folha");
+        var img_uchiha=document.querySelector(".img-uchiha");
+        var img_akatsuki=document.querySelector(".img-akatsuki");
+        var experience=document.querySelector(".experience");
+        var education=document.querySelector(".education");
+        var title_skills=document.querySelector(".title-skills");
+
+        if(body.classList.contains("light-mode")){
+           
+            img_mode.src="img/rinegan.png";
+            img_folha.src="img/naruto/dark-mode/simbolo da folha.png";
+            img_akatsuki.src="img/naruto/dark-mode/akatsuki.png";
+            img_uchiha.src="img/naruto/dark-mode/cla uchiha.png";
+            experience.classList.add("bg-info");
+            education.classList.add("bg-info");
+            // title_skills.classList.replace("text-danger","text-info");
+            body.classList.replace("light-mode","dark-mode");
+            return
+        }else{
+           
+            img_mode.src="img/mangeki sharinga.png";
+            img_folha.src="img/naruto/ligth-mode/simbolo da folha.png";
+            img_akatsuki.src="img/naruto/ligth-mode/akatsuki.png";
+            img_uchiha.src="img/naruto/ligth-mode/cla uchiha.png";
+            experience.classList.remove("bg-info");
+            education.classList.remove("bg-info");
+            // title_skills.classList.replace("text-info","text-danger");
+            body.classList.replace("dark-mode","light-mode");  
+        }
     });
+// function dark_mode(mode){
+//     if(mode=="dark-mode"){
+
+//     }
+// }
